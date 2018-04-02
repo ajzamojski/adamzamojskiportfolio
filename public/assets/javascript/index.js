c7e4ccfafda939634c35f379b593631726ff8539
Adam Zamojski <ajzamojski@gmail.com>
2018-04-02 11:41:40 -0400

    new responsiveness for project visibility

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -1,71 +1,50 @@
 
-// $(window).resize(function () {
-//     var viewportWidth = $(window).width();
-    // if (viewportWidth > 892) {
-    //       $("#navbarNav").addClass("col-5");
-    // }
-    // if (viewportWidth < 892) {
-    //      	$("#navbarNav").removeClass("col-5 collapse");
-    // }
-//     if (true) {}
-// });
-
-// $("#buttondropnav").click(function () {
-// 	let buttonOpen = false;
-// 	// if ( $("#buttondropnav").css('display') == 'none') {
-// 		if (!buttonOpen) {
-// 			$("#navbarNav").removeClass("col-5", function(){
-// 					buttonOpen = true;
-// 					return;
-// 				});
-// 		}
-// 		if (buttonOpen) {
-// 			$("#navbarNav").addClass("col-5", function(){
-// 				buttonOpen = false;
-// 				return;
-// 				});
-// 		}
-// 	// }
-// });
 
 // mouseover effects for portfolio tab
 $(document).ready(function() {
+	// if ($(window).width() > 767) {
 	// **************** Portfolio hover effects ********************
-	$('.wrapper').mouseover(function() {
-			// console.log("mousein");
-		  $(this).find("img").css({
-		  	"opacity" : ".3"
-		  });
-		  $(this).find("span").css({
-		  	"visibility" : "visible"
-		  });
+		$('.wrapper').mouseover(function() {
+			if ($(window).width() > 752) {
+					console.log(this);
+			  $(this).find("img").css({
+			  	"opacity" : ".3"
+			  });
+			  $(this).find("span").css({
+			  	"visibility" : "visible"
+			  });
+			} 
 		});
-	$('.wrapper').mouseout(function() {
-		// console.log("this is a mouseout");
-		$(this).find("img").css({
-			"transition": "all .5s ease-out",
-			"opacity" : "1"
+		$('.wrapper').mouseout(function() {
+			if ($(window).width() > 752) {
+				$(this).find("img").css({
+					"transition": "all .5s ease-out",
+					"opacity" : "1"
+				});
+				$(this).find("span").css({
+					"transition": "all .1s ease-out",
+					"visibility" : "hidden"
+				});
+			}
 		});
-		$(this).find("span").css({
-			"transition": "all .1s ease-out",
-			"visibility" : "hidden"
+
+		$('.iconpics').mouseover(function() {
+			$(this).css({
+				'-webkit-filter' : 'none',
+	  		'filter' : 'none'
+			})
 		});
-	});
 
-	$('.iconpics').mouseover(function() {
-		$(this).css({
-			'-webkit-filter' : 'none',
-  		'filter' : 'none'
-		})
-	});
-
-	$('.iconpics').mouseout(function() {
-		$(this).css({
-			'-webkit-filter' : 'grayscale(100%)',
-  		'filter' : 'grayscale(100%)'
-		})
-	});
-
+		$('.iconpics').mouseout(function() {
+			$(this).css({
+				'-webkit-filter' : 'grayscale(100%)',
+	  		'filter' : 'grayscale(100%)'
+			})
+		});
+	
+	// else {
+	// 	console.log("window width is small");
+	// }
 });
 
 
