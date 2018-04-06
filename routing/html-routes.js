fb42e14e851b217f3bad3941e25c533aea8263e6
Adam Zamojski <ajzamojski@gmail.com>
2018-04-05 20:11:23 -0400

    updated OAuth2 for gmail connection

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require("path");
 const nodeMailer = require('nodemailer');
 
@@ -23,30 +24,19 @@ module.exports = function(app) {
 	});
 
 	app.post("/send_form_email", function(req, res) {
-	// 	console.log("server route hit");
-	// 	app.use(path.join(__dirname, "/../public/send_form_email.php"));
-	// });
-	// app.post('/send_form_email', function(req, res){
-	// 	console.log("send_form_email route hit");
-	// 	exec("php -S /public/send_form_email.php", function (error, stdout, stderr) {
-	// 		console.log("file read php");
-	// 		console.log(stdout);
-	// 		res.send(stdout);
-	// 	});
 
 		console.log(req.body);
 			let transporter = nodeMailer.createTransport({
 				host: 'smtp.gmail.com',
-			  port: 465,
-			  secure: true,
-			  auth : {
-			  	type: 'OAuth2',
-			    user: "ajzamojski@gmail.com",
-			    clientId: "832250632334-9orhrsu3j2ors7a2s4mrsn4rfccqlh0v.apps.googleusercontent.com",
-			    clientSecret: "FlsHetg3NaophIAkTvCHKOEM",
-			    refreshToken: "ya29.GluTBaWc0dkxGa8l4uG9jvbvWgU-tYtzk5IakI9-D35ZpoZWs7YCTYGRnUqcqdxLmOTMzgwhDdwXWlVvOnwD2m07OoHZmPAh_64DrEg25gnApljRVmPtUMU_7QLW",
-			    accessToken: 'ya29.GluTBaWc0dkxGa8l4uG9jvbvWgU-tYtzk5IakI9-D35ZpoZWs7YCTYGRnUqcqdxLmOTMzgwhDdwXWlVvOnwD2m07OoHZmPAh_64DrEg25gnApljRVmPtUMU_7QLW'
-	      }
+				port: 465,
+				secure: true,
+			  auth: {
+			  		type: 'OAuth2',
+			  		user: process.env.MY_EMAIL,
+			      clientId: process.env.CLIENT_ID,
+			      clientSecret: process.env.CLIENT_SECRET,
+			      refreshToken: process.env.REFRESH_TOKEN
+				}
 			});
 
 			let mailOptions = 
